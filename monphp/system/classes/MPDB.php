1944dd34125ae1f357e1e85758c0cc081229f359
Jason T. Wong <developer@jasontwong.com>
2012-05-18 17:14:16 -0400

    MPDB class     - Fixed function whitespacing

--- monphp/system/classes/MPDB.php
+++ monphp/system/classes/MPDB.php
@@ -6,8 +6,8 @@ class MPDB
     private $conn;
     private $db;
     // }}}
-    // {{{ function __construct ($set = 'default')
-    function __construct ($set = 'default', $db_file = '')
+    // {{{ function __construct($set = 'default')
+    function __construct($set = 'default', $db_file = '')
     {
         include $db_file === ''
             ? DIR_SYS.'/config.database.php'
@@ -16,8 +16,8 @@ class MPDB
         $this->db = $this->conn->selectDB($_db_conn[$set]['options']['db']);
     }
     // }}}
-    // {{{ public function __call ($name, $arguments)
-    public function __call ($name, $arguments)
+    // {{{ public function __call($name, $arguments)
+    public function __call($name, $arguments)
     {
         if (method_exists($this->db, $name))
         {
@@ -26,8 +26,8 @@ class MPDB
         throw new MPDBException('That method does not exist');
     }
     // }}}
-    // {{{ public static function __callStatic ($name, $arguments)
-    public static function __callStatic ($name, $arguments)
+    // {{{ public static function __callStatic($name, $arguments)
+    public static function __callStatic($name, $arguments)
     {
         $mondb = new MPDB();
         $db = $mondb->db;
@@ -38,8 +38,8 @@ class MPDB
         throw new MPDBException('That method does not exist');
     }
     // }}}
-    // {{{ public function __get ($name)
-    public function __get ($name)
+    // {{{ public function __get($name)
+    public function __get($name)
     {
         return $this->db->$name;
     }
