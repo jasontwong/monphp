44e21716f1772e44b7c33c22e66ee36afcf94440
Jason T. Wong <jason@kratedesign.com>
2012-11-16 10:05:58 -0500

    MPFileManager     - Converting towards using the Mongo GridFS

--- monphp/module/MPFileManager/module.php
+++ monphp/module/MPFileManager/module.php
@@ -369,6 +369,7 @@ class MPFileManager
             ),
             'name' => 'file_path',
             'type' => 'text',
+            'placeholder' => DIR_FILE . '/upload'
             'value' => array(
                 'data' => self::$file_path
             )
@@ -500,79 +501,7 @@ class MPFileManager
     {
         $original_file = $path.'/'.$name;
         $success = move_uploaded_file($tmp_file, $original_file);
-
-        if ($success)
-        {
-            $resized_path = $path.'/_resized';
-            mkdir($resized_path);
-            if (is_dir($resized_path) && list($width, $height, $mime_type) = getimagesize($original_file))
-            {
-                $quality = 90;
-                $pinfo = pathinfo($name);
-                foreach (self::$sizes as $label => $size)
-                {
-                    if ($size['width'] > 0 && $size['height'] > 0 && ($width > $size['width'] || $height > $size['height']))
-                    {
-                        $ratio_orig = $width/$height;
-
-                        if ($size['width']/$size['height'] > $ratio_orig) 
-                        {
-                           $size['width'] = $size['height']*$ratio_orig;
-                        } 
-                        else 
-                        {
-                           $size['height'] = $size['width']/$ratio_orig;
-                        }
-                        
-                        $image = imagecreatetruecolor($size['width'], $size['height']);
-                        $resized_file = $resized_path.'/'.$pinfo['filename'].'-'.$label.'.'.$pinfo['extension'];
-                        switch ($mime_type)
-                        {
-                            case IMAGETYPE_GIF:
-                                $orig_image = imagecreatefromgif($original_file);
-                                imagealphablending($image, false);
-                                imagesavealpha($image, true);
-                                imagecopyresampled($image, $orig_image, 0, 0, 0, 0, $size['width'], $size['height'], $width, $height);
-                                imagegif($image, $resized_file);
-                            break;
-                            case IMAGETYPE_JPEG:
-                                $orig_image = imagecreatefromjpeg($original_file);
-                                imagecopyresampled($image, $orig_image, 0, 0, 0, 0, $size['width'], $size['height'], $width, $height);
-                                imagejpeg($image, $resized_file, $quality);
-                            break;
-                            case IMAGETYPE_PNG:
-                                $orig_image = imagecreatefrompng($original_file);
-                                imagealphablending($image, false);
-                                imagesavealpha($image, true);
-                                imagecopyresampled($image, $orig_image, 0, 0, 0, 0, $size['width'], $size['height'], $width, $height);
-                                if ( function_exists('imageistruecolor') && imageistruecolor( $orig_image ) )
-                                {
-                                    imagetruecolortopalette( $image, false, imagecolorstotal( $orig_image ) );
-                                }
-                                imagepng($image, $resized_file);
-                            break;
-                            case IMAGETYPE_WBMP:
-                                $orig_image = imagecreatefromwbmp($original_file);
-                                imagecopyresampled($image, $orig_image, 0, 0, 0, 0, $size['width'], $size['height'], $width, $height);
-                                imagewbmp($image, $resized_file);
-                            break;
-                            case image_type_to_mime_type(IMAGETYPE_XBM):
-                                $orig_image = imagecreatefromxbm($original_file);
-                                imagecopyresampled($image, $orig_image, 0, 0, 0, 0, $size['width'], $size['height'], $width, $height);
-                                imagexbm($image, $resized_file);
-                            break;
-                        }
-                        if (isset($orig_image))
-                        {
-                            imagedestroy($orig_image);
-                        }
-                        imagedestroy($image);
-                    }
-                }
-            }
-        }
-
-        return $success;
+        return MPFile::save_image($original_file, array(), self::$sizes);
     }
     // }}}
 
